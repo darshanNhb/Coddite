a7303081eba0d574ecea89cbf771fd722f0044f0
darshanNhb <buddhdevdarshan1478@gmail.com>
2026-08-19 23:26:59 +0530

    feat: add Prisma database client configuration

---
+++ backend_of_login_signup/src/config/prisma.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: true,
+    },
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({
+    adapter,
+});
+
+export default prisma;
